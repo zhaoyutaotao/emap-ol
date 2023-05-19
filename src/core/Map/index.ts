f44645b4c404e635b8e39efdcd41433287e68e07
zhaoyutao <1048@163.com>
2023-05-19 18:11:14 +0800

    添加测试typedoc

---
+++ src/core/Map/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Map地图核心类
+ * @Author: zhaoyutao 1048@163.com
+ * @Date: 2023-05-19 10:42:31
+ * @LastEditors: zhaoyutao 1048@163.com
+ * @Description: 
+ */
+export default class Map{
+    constructor(){
+        const title:string = 'emap'
+        console.log(title)
+    }
+}
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,2 +1,7 @@
-const title = 'emap-ol'
-console.log(title)
+/*
+ * @Author: zhaoyutao 1048@163.com
+ * @Date: 2023-05-17 16:49:10
+ * @LastEditors: zhaoyutao 1048@163.com
+ * @Description: 
+ */
+export { default as Map } from './Map'
